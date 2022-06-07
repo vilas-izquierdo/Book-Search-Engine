a815d9ae2c77b6a58813e7960c80c5a4db335f3b
Vilasa Izquierdo <vilasi@gmail.com>
2022-06-06 17:32:26 -0700

    type defs

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID!
+    authors: String
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    users: User
+  }
+
+  type Query {
+    profiles: [Profile]!
+    profile(profileId: ID!): Profile
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
